56ad539757e31e3ad708b03d838012b82615b449
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-03-29 22:12:04 +0800

    setter method in the class and config context.xml to inject literal values

--- src/com/course/springdemo/CricketCoach.java
+++ src/com/course/springdemo/CricketCoach.java
@@ -2,7 +2,27 @@ package com.course.springdemo;
 
 public class CricketCoach implements Coach {
 
+    private String emailAddress;
+    private String team;
     private FortuneService fortuneService;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
+    }
 
     // create a no-arg constructor
     public CricketCoach() {
